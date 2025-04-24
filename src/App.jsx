7c2f74930c2adb37d0a19b6b1d6a17a6ad55b644
Alex <alex.wangzhe0809@gmail.com>
2025-04-25 00:38:38 +0200

    mainpage padding

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Main from "./components/Main";
 
 function App() {
   return (
-    <div className="app p-4">
+    <div className="app p-4 lg:py-20 lg:px-40 w-max-[1440px] w-min-[375px]">
       <Header />
       <Main />
     </div>
